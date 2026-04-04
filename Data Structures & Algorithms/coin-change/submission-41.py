3ebd06c6bb1769521b807fd84a6b0ce7380c7a5f
openbinary <242321830+openbinary0@users.noreply.github.com>
2026-04-04 20:35:45 +0000

    Add: coin-change - submission-41

---
+++ Data Structures & Algorithms/coin-change/submission-41.py
@@ -0,0 +1,20 @@
+import math
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        def dp(coins, amount):
+            # base case:
+            if amount < 0:
+                return math.inf
+            if amount == 0:
+                return 0
+            if len(coins) == 1:
+                return dp(coins, amount - coins[0])
+            # recursive case:
+            return min(1 + dp(coins, amount - coins[0]), dp(coins[1:], amount))
+            # two operations:
+            # * grab the coin and stay
+            # * move
+        
+        result = dp(coins, amount)
+        return result if result != math.inf else -1
