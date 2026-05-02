ddc1c030c66141a1e6902f5da6e71aeb823847d6
openbinary <242321830+openbinary0@users.noreply.github.com>
2026-05-02 04:05:21 +0000

    Add: regular-expression-matching - submission-7

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-7.py
@@ -0,0 +1,22 @@
+def get_length(s, i):
+    length = len(s) - i
+    return length if length >= 0 else 0
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        def is_match(i, j):
+            # base case:
+            if not get_length(s, i) and not get_length(p, j):
+                return True
+            if get_length(s, i) and not get_length(p, j):
+                return False
+            # recursive case (p guaranteed contain characters):
+            if get_length(p, j) >= 2 and p[j+1] == "*":
+                if get_length(s, i) and (s[i] == p[j] or p[j] == "."):
+                    return is_match(i+1, j) or is_match(i, j+2)
+                # only one choice:
+                return is_match(i, j+2)
+            elif get_length(s, i) and (s[i] == p[j] or p[j] == "."):
+                return is_match(i+1, j+1)
+            return False
+        return is_match(0, 0)
